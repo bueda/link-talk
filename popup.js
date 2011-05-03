f8a50e96d35edf3086b316196c6ac9ad296b1044
Christopher Peplin <peplin@bueda.com>
2011-05-02 23:54:09 -0400

    Send URL to the new URl field directly.

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 function onLoad() {
     chrome.tabs.getSelected(null, function(tab) {
-        chrome.tabs.create({"url": 'https://www.bueda.com/posts/new/?reply=' +
+        chrome.tabs.create({"url": 'https://www.bueda.com/posts/new/?url=' +
                     encodeURIComponent(tab.url) + '&subject=' +
                     encodeURIComponent(tab.title),
                 "selected": true});
